d404a4faa46549b1bd84e7c46cc8844b7eb74255
Nacho <nachthelad@gmail.com>
2023-12-30 13:10:58 -0300

    fixed drawer color

--- core/components/MenuBar/index.tsx
+++ core/components/MenuBar/index.tsx
@@ -27,51 +27,51 @@ const monedas = [
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#54a200',
+      main: '#008000',
     },
   },
 });
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
+// const Search = styled("div")(({ theme }) => ({
+//   position: "relative",
+//   borderRadius: theme.shape.borderRadius,
+//   backgroundColor: alpha(theme.palette.common.white, 0.15),
+//   "&:hover": {
+//     backgroundColor: alpha(theme.palette.common.white, 0.25),
+//   },
+//   marginLeft: 0,
+//   width: "100%",
+//   [theme.breakpoints.up("sm")]: {
+//     marginLeft: theme.spacing(1),
+//     width: "auto",
+//   },
+// }));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+// const SearchIconWrapper = styled("div")(({ theme }) => ({
+//   padding: theme.spacing(0, 2),
+//   height: "100%",
+//   position: "absolute",
+//   pointerEvents: "none",
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "center",
+// }));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  width: "100%",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
+// const StyledInputBase = styled(InputBase)(({ theme }) => ({
+//   color: "inherit",
+//   width: "100%",
+//   "& .MuiInputBase-input": {
+//     padding: theme.spacing(1, 1, 1, 0),
+//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+//     transition: theme.transitions.create("width"),
+//     [theme.breakpoints.up("sm")]: {
+//       width: "12ch",
+//       "&:focus": {
+//         width: "20ch",
+//       },
+//     },
+//   },
+// }));
 
 export default function SearchAppBar({ onFilter }: SearchAppBarProps) {
   const [drawerOpen, setDrawerOpen] = React.useState(false);
@@ -141,15 +141,6 @@ export default function SearchAppBar({ onFilter }: SearchAppBarProps) {
           >
             verdurita
           </Typography>
-          {isMobile ? null : monedas.map((moneda) => (
-            <Button
-              key={moneda}
-              onClick={() => onFilter(moneda)}
-              sx={{ color: 'white' }}
-            >
-              {moneda}
-            </Button>
-          ))}
           <Button
             onClick={() => onFilter()}
             variant="outlined"
@@ -166,6 +157,15 @@ export default function SearchAppBar({ onFilter }: SearchAppBarProps) {
             {" "}
             Todas
           </Button>
+          {isMobile ? null : monedas.map((moneda) => (
+            <Button
+              key={moneda}
+              onClick={() => onFilter(moneda)}
+              sx={{ color: 'white' }}
+            >
+              {moneda}
+            </Button>
+          ))}
         </Toolbar>
       </AppBar>
       <Drawer
