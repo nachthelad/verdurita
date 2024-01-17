b46e7315146c14a4ed150353b21b9567664a271c
Nacho <nachthelad@gmail.com>
2024-01-17 16:55:22 -0300

    Agregado Footer y nuevo width a cards

--- core/components/CardItem/index.tsx
+++ core/components/CardItem/index.tsx
@@ -1,31 +1,33 @@
-  import * as React from 'react';
-  import Card from '@mui/material/Card';
-  import CardContent from '@mui/material/CardContent';
-  import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
-  interface CardItemProps {
-    texto: string;
-    precio: number;
-  }
+interface CardItemProps {
+  texto: string;
+  precio: number;
+}
 
-  const cardStyle = {
-    maxWidth: '450px',
-    margin: 'auto', 
-  };
+const CardItem = ({ texto, precio }: CardItemProps) => {
+  return (
+    <Card
+      sx={{
+        maxWidth: { xs: "100%", sm: "450px" },
+        minWidth: 180,
+        mx: 0,
+        boxShadow: 3,
+      }}
+    >
+      <CardContent sx={{ padding: 2 }}>
+        <Typography variant="h5" component="div">
+          {texto}
+        </Typography>
+        <Typography variant="h5" color="text.secondary">
+          ${precio.toFixed(2).replace(".", ",")}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
-  const CardItem = ({texto, precio}: CardItemProps) => {
-    return (
-      <Card sx={{ ...cardStyle, minWidth: 180  }}>
-        <CardContent>
-          <Typography variant="h5" component="div">
-            {texto}
-          </Typography>
-          <Typography variant="h5" color="text.secondary">
-          ${precio.toFixed(2).replace('.', ',')}
-          </Typography>
-        </CardContent>
-      </Card>
-    )
-  }
-
-  export default CardItem
+export default CardItem;
--- core/components/Footer/index.tsx
+++ core/components/Footer/index.tsx
@@ -4,15 +4,15 @@ import { Box, Typography, Link } from "@mui/material";
 const Footer: React.FC = () => {
   return (
     <Box component="footer" sx={{ padding: 2, textAlign: "center" }}>
-      <Typography variant="body2" color="white">
+      <Typography variant="body2" color="white" sx={{ whiteSpace:'nowrap' }}>
         Copyright ©{" "}
         <Link
           href="https://nachthelad.com.ar"
           target="_blank"
           rel="noopener noreferrer"
           sx={{
-            color: "white", // Cambia el color del texto a blanco
-            textDecoration: "none", // Elimina el subrayado
+            color: "white", 
+            textDecoration: "none", 
           }}
         >
           nachthelad.com.ar
