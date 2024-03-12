f1648b278355778251b5b0cba57d4fbcb24e8115
Nacho <nachthelad@gmail.com>
2024-03-12 18:20:31 -0300

    Dialog transform

--- core/components/CardItem/ModalCardItem.tsx
+++ core/components/CardItem/ModalCardItem.tsx
@@ -56,7 +56,7 @@ const ModalCardItem = ({
         style: {
           borderRadius: "20px",
           overflowY: "auto",
-          transform: isKeypadOpen ? "translateY(-50%)" : "none",
+          transform: isKeypadOpen ? "translateY(-50%)" : "none", // Move up the dialog when the keypad is open
         },
       }}>
       <Card
